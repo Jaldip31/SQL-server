1e0848c74a3b1c2861e45586be0eaa12135097e9
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-02-15 14:59:23 +0530

    15-02-200

--- sql/query/2_query.sql
+++ sql/query/2_query.sql
@@ -21,7 +21,24 @@ inner join product_master on product_master.productno=sales_order_detail.product
 
 where sales_order.delydate like '%-07-%'
 
+--4
+select client_master.clientname
+from client_master
+inner join sales_order on client_master.clientNo=sales_order.clientNo
+inner join sales_order_detail on sales_order_detail.orderno=sales_order.orderno
+inner join product_master on product_master.productno=sales_order_detail.productno
 
+where product_master.description = 'Trousers'
+
+--5
+select product_master.description,
+sales_order_detail.qtyorderd
+from client_master
+inner join sales_order on client_master.clientNo=sales_order.clientNo
+inner join sales_order_detail on sales_order_detail.orderno=sales_order.orderno
+inner join product_master on product_master.productno=sales_order_detail.productno
+
+where product_master.description = 'Pull Overs'
 --6
 select client_master.clientname,
 		product_master.description ,
