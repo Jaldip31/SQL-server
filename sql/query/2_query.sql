b39e358d64032f7ae179b6165a9a289129fdee0a
jaldip bhalani <92107664+Jaldip31@users.noreply.github.com>
2022-02-16 13:24:35 +0530

    16-02-2022

--- sql/query/2_query.sql
+++ sql/query/2_query.sql
@@ -1,6 +1,6 @@
 --1
 select client_master.clientname,
-		product_master.description 
+	product_master.description 
 	
 from client_master
 inner join sales_order on client_master.clientNo=sales_order.clientNo
@@ -10,9 +10,8 @@ inner join product_master on product_master.productno=sales_order_detail.product
 where client_master.clientname = 'Ivan Baross'
 
 --2
-select 
-		product_master.description ,
-		sales_order_detail.qtyorderd 
+select product_master.description ,
+	sales_order_detail.qtyorderd 
 
 from client_master
 inner join sales_order on client_master.clientNo=sales_order.clientNo
@@ -32,17 +31,18 @@ where product_master.description = 'Trousers'
 
 --5
 select product_master.description,
-sales_order_detail.qtyorderd
+	sales_order_detail.qtyorderd
 from client_master
 inner join sales_order on client_master.clientNo=sales_order.clientNo
 inner join sales_order_detail on sales_order_detail.orderno=sales_order.orderno
 inner join product_master on product_master.productno=sales_order_detail.productno
 
 where product_master.description = 'Pull Overs'
+
 --6
 select client_master.clientname,
-		product_master.description ,
-		sales_order_detail.qtyorderd
+	product_master.description ,
+	sales_order_detail.qtyorderd
 
 from client_master
 inner join sales_order on client_master.clientNo=sales_order.clientNo
@@ -53,8 +53,8 @@ where client_master.clientname in ('Ivan Baross','Chhaya Bankar')
 
 --7
 select client_master.clientname,
-		product_master.description ,
-		sales_order_detail.qtyorderd
+	product_master.description ,
+	sales_order_detail.qtyorderd
 
 from client_master
 inner join sales_order on client_master.clientNo=sales_order.clientNo
